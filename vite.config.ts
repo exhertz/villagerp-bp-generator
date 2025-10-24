241ec4894443c3807c6f5ff1d22a092fc5df4e39
exhertz <cactuseveryday@tuta.io>
2025-10-24 06:05:38 +0300

    build: implemented assembly into one .html file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { viteSingleFile } from "vite-plugin-singlefile"
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [svelte(), tailwindcss()],
+  plugins: [svelte(), tailwindcss(), viteSingleFile()],
   server: {
     host: "0.0.0.0"
+  },
+  build: {
+    target: 'esnext',
+    modulePreload: false,
+    polyfillModulePreload: false,
   }
 })
